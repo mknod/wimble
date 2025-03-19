78124f06033c3effb15025f163ed0b8802a6fe04
jr <jr@rmrn.net>
2025-03-19 17:56:19 -0400

    Working, a little.

--- src/config.rs
+++ src/config.rs
@@ -1,23 +1,93 @@
+use config::{Config, ConfigError, File};
+use serde::Deserialize;
+//use serde_derive::Deserialize;
 use std::sync::OnceLock;
-use std::fs;
-use std::path::Path;
-use toml::Value;
 
-static CONFIG: OnceLock<Value> = OnceLock::new();
+// #[derive(Debug, Deserialize)]
+// #[allow(unused)]
+// struct Global {
+    
+// }
 
-fn load_config() -> &'static Value {
-    CONFIG.get_or_init(|| {
-        let config_content = fs::read_to_string(Path::new("config.toml"))
-            .expect("Failed to read config.toml");
-        config_content.parse::<Value>()
-            .expect("Failed to parse config.toml")
-    })
+static CONFIG: OnceLock<Settings> = OnceLock::new();
+
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+pub struct Browser {
+   pub enabled: bool,
+    start_url: String,
+    start_cmd: String,
+
 }
 
-pub fn get_config() -> &'static Value {
-    load_config()
-
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+pub struct Streambot {
+    enabled: bool,
+    channel: String,
+    username: String,
+    command_symbol: String,
+    client_id: String,
+    access_token: String,
+    refresh_token: String,
 }
 
 
+impl Streambot {
+    pub fn enabled(&self) -> bool {
+        self.enabled
+    }
+    
+    pub fn channel(&self) -> &str {
+        &self.channel
+    }
+    
+    pub fn username(&self) -> &str {
+        &self.username
+    }
+    
+    pub fn command_symbol(&self) -> &str {
+        &self.command_symbol
+    }
+    
+    pub fn client_id(&self) -> &str {
+        &self.client_id
+    }
+    
+    pub fn access_token(&self) -> &str {
+        &self.access_token
+    }
+    
+    pub fn refresh_token(&self) -> &str {
+        &self.refresh_token
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+pub struct Settings {
+ //   pub(crate) global: Global,
+    pub(crate) browser: Browser,
+    pub(crate) streambot: Streambot,
+}
+
+impl Settings {
+pub fn new() -> &'static Settings {
+    CONFIG.get_or_init(|| 
+        Config::builder()
+        .add_source(File::with_name("config"))
+        .build()
+        .unwrap()
+        .try_deserialize()
+        .unwrap()
+    )
+}
+
+pub fn streambot(&self) -> &Streambot {
+    &self.streambot
+}
+
+
+}
 
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,44 @@
-mod config;
-mod browser;
-mod streambot;
+// Import from your own library crate
+use wimble::streambot::Streambot;
+use wimble::config::Settings;
 
 #[tokio::main]
 async fn main() {
-     // Should check that these are enabled in config.toml before starting them. 
-  start_browser().await;
-  start_streambot().await;
+    let settings = Settings::new();
+    println!("Settings: {:?}", settings);
+
+    let streambot = Streambot::new(
+        settings.streambot().channel().to_owned(),
+        settings.streambot().username().to_owned(),
+        settings.streambot().command_symbol().to_owned(),
+        settings.streambot().access_token().to_owned(),
+    );
+
+    streambot.start_streambot().await;
+
 }
 
 
-async fn start_browser() -> Option<()> {
-     if browser::init_driver().await.is_err() {
-          return None;
-     }
-     if browser::browser().await.is_err() {
-          return None;
-     }
-     Some(())
-     //let _ = browser::browser_command("Up".to_string());
-}
+// async fn start_browser() -> Option<()> {
+//      if browser::init_driver().await.is_err() {
+//           return None;
+//      }
+//      if browser::browser().await.is_err() {
+//           return None;
+//      }
+//      Some(())
+//      //let _ = browser::browser_command("Up".to_string());
+// }
 
-async fn start_streambot() {
-     streambot::streambot().await;
-}
+// async fn start_streambot() {
+//      //streambot::streambot().await;
+// }
 
 // If a command is recieved from the streambot, send it to the browser
-async fn send_command(command: String) {
-     let _ = browser::browser_command(command).await;
-}
+//async fn send_command(command: String) {
+//     let _ = browser::browser_command(command).await;
+//}
 
-async fn send_chat_message(message: String) {
-     let _ = streambot::send_chat_message(message).await;
-}
+//async fn send_chat_message(message: String) {
+//     let _ = streambot::send_chat_message(message).await;
+//}
